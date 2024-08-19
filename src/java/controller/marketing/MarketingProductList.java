b84976066ec5cc38eca45674e689f33e2287029f
Nguyễn Xuân Tâm <xuantamjee413@gmail.com>
2024-08-20 03:32:11 +0700

    ud

--- src/java/controller/marketing/MarketingProductList.java
+++ src/java/controller/marketing/MarketingProductList.java
@@ -1,3 +1,4 @@
+
 package controller.marketing;
 
 import controller.user.BaseRequiredAuthenticationController;
@@ -14,9 +15,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.util.List;
 
-@WebServlet(name = "ProductListController", urlPatterns = {"/marketing/productlist", "/marketing/changestatus"})
-public class ProductListController extends HttpServlet {
-
+public class MarketingProductList extends HttpServlet {
     private static final int PAGE_SIZE = 10;
 
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
@@ -73,25 +72,40 @@ public class ProductListController extends HttpServlet {
         }
     }
 
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /** 
+     * Handles the HTTP <code>GET</code> method.
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+    throws ServletException, IOException {
         processRequest(request, response);
-    }
+    } 
 
+    /** 
+     * Handles the HTTP <code>POST</code> method.
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+    throws ServletException, IOException {
         processRequest(request, response);
     }
 
-//    @Override
-//    protected void doPost(HttpServletRequest req, HttpServletResponse resp, Account account) throws ServletException, IOException {
-//        processRequest(req, resp);
-//    }
-//
-//    @Override
-//    protected void doGet(HttpServletRequest req, HttpServletResponse resp, Account account) throws ServletException, IOException {
-//        processRequest(req, resp);
-//    }
+    /** 
+     * Returns a short description of the servlet.
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
 }
