4fbba6a6448dfcf9821c17312b433eb3b73bb579
S1nsoledad <hieupu2003@gmail.com>
2024-08-23 22:05:57 +0700

    fix insert

--- src/java/dal/UserDBContext.java
+++ src/java/dal/UserDBContext.java
@@ -54,7 +54,7 @@ public class UserDBContext extends DBContext<User> {
                                  ,[dob]
                                  ,[status]
                                  ,[fullname]
-                                 ,[avar]
+                                 ,[ava]
                                  ,[createdate])
                            VALUES
                                  (?,?,?,?,?,?,?,?,?,?)""";
