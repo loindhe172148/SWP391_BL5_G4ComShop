9f4db42a8481e7d33fa42dc60479fddd9de8a5f6
truongtqhe172652 <truongtqhe172652@fpt.edu.vn>
2024-08-25 02:33:58 +0700

    update project

--- src/java/dal/DBContext.java
+++ src/java/dal/DBContext.java
@@ -17,7 +17,7 @@ public abstract class DBContext<T> {
     public DBContext() {
         try {
             String user = "sa";
-            String pass = "ductung1706@";
+            String pass = "123456";
             String url = "jdbc:sqlserver://localhost:1433;databaseName=G4COMShop;encrypt=true;trustServerCertificate=true;";
             Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
             connection = DriverManager.getConnection(url, user, pass);
